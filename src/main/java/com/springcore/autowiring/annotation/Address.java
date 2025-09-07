3d6b5e50b45b17069e832de3e4b800ee03c8d74e
Rahul <rahulprem02@gmail.com>
2025-09-07 20:13:26 +0200

    Day 6: Autowiring with XML modes and annotations

---
+++ src/main/java/com/springcore/autowiring/annotation/Address.java
@@ -0,0 +1,29 @@
+package com.springcore.autowiring.annotation;
+
+public class Address {
+
+ private String street;
+ private String bus;
+ public String getStreet() {
+    return street;
+ }
+ public void setStreet(String street) {
+    this.street = street;
+ }
+ public String getBus() {
+    return bus;
+ }
+ public void setBus(String bus) {
+    this.bus = bus;
+ }
+ @Override
+ public String toString() {
+    return "Address [street=" + street + ", bus=" + bus + "]";
+ }
+
+ 
+
+
+ 
+
+}
